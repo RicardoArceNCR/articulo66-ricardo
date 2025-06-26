b0308433099f24ee8a80cd240dd844d3a27c322a
RicardoArceNCR <116515519+RicardoArceNCR@users.noreply.github.com>
2025-06-26 10:31:37 -0600

    WIP: ajustes en CSS y header - Nuevas clases bases de colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,24 @@ export default {
   content: [
     './index.php',
     './app/**/*.php',
+    './resources/views/**/*.blade.php',
     './resources/**/*.{php,vue,js}',
   ],
   theme: {
     extend: {
+      colors: {
+        'grey-1': '#F4F9FC',
+        'grey-2': '#E7E7E7',
+        'dark-100': '#5D5D5D',
+        'dark-50': '#333333',
+        blue:      '#1D447A',
+        'green-100':'#0A95A6',
+        'green-50': '#1BC6EB',
+      },
       fontFamily: {
         sans: ['Raleway', 'sans-serif'],
       },
-      colors: {
-        'primary': '#0066CC',
-      }
     },
   },
   plugins: [],
-} 
+}
